db57c6207ffc7a3e465adc1f5aa012d32eeb3d49
Gustav Tschirschnitz <gustavtschirschnitz@googlemail.com>
2026-06-20 13:30:33 +0100

    feat: remove durationHours pref and run windows from types/stores

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -24,30 +24,18 @@ export interface ScoredHour extends HourData {
   score: number;
 }
 
-export interface RunWindow {
-  startHour: number;
-  endHour: number;
-  startTime: string;
-  endTime: string;
-  score: number;
-  hours: ScoredHour[];
-  scores: number[];
-}
-
 export interface DayData {
   date: string;
   dayIndex: number;
   hours: ScoredHour[];
   scores: number[];
   bestScore: number;
-  windows: RunWindow[];
 }
 
 export interface Preferences {
   rainTolerance: number;
   tempMin: number;
   tempMax: number;
-  durationHours: number;
 }
 
 export interface WeatherInfo {
